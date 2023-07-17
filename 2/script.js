123a0ea664e790988ed412973e84f58e6d61eca9
LAUVM10 <ing.lauvm@gmail.com>
2023-07-16 19:17:26 -0500

    save

---
+++ 2/script.js
@@ -0,0 +1,8 @@
+// Solicitar al usuario ingresar un string con palabras separadas por comas
+const string = prompt("Ingrese una secuencia de caracteres con palabras separadas por comas:");
+
+// Convertir el string en un array
+const arrayString = string.split(",");
+
+// Mostrar el resultado en la consola
+console.log(arrayString);
